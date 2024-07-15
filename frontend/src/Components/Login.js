2a1320a83a49e503bad3097b20bf3d9dc8e2e078
Aleksandra Brodowska <s25684@pjwstk.edu.pl>
2024-07-15 18:56:13 +0200

    login/register

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -3,22 +3,21 @@ import '../Styles/Auth.css';
 
 function Login() {
   return (
-    <div className="container">
-        <div className="auth-container">
-            <div className="auth-header">Logowanie</div>
-            <div className="auth-form">
-                <div className="form-group">
-                    <label htmlFor="email">*E-mail:</label>
-                    <input type="email" id="email" name="email" required />
-                </div>
-                <div className="form-group">
+    <div className="auth-container">
+        <div className="auth-header">Logowanie</div>
+        <form className="auth-form">
+            <div className="labels">
+                <label htmlFor="email">*E-mail:</label>
                 <label htmlFor="password">*Hasło:</label>
+            </div>
+
+            <div className="inputs">
+                <input type="email" id="email" name="email" required />
                 <input type="password" id="password" name="password" required />
-                </div>
                 <button className="auth-button">Zaloguj</button>
                 <a href="#" className="forgot-password">Nie pamiętam hasła</a>
             </div>
-        </div>
+        </form>
     </div>
   );
 }

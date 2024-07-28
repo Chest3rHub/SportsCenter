7fd9e81c7475a0d9d78be422aad613429d9757b3
Szymon Sawicki <darhmoule@interia.pl>
2024-07-28 20:23:04 +0200

    changed register structure to table and styled

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -11,30 +11,44 @@ function Register() {
         <Header>Rejestracja</Header>
         <OrangeBackground>
         <form>
-          <div className="register-content">
-          <div className="labels">
-            <div className='top-labels'>
-            <label htmlFor="firstName">*Imię:</label>
-            <label htmlFor="lastName">*Nazwisko:</label>
-            <label htmlFor="address">Adres:</label>
-            <label htmlFor="birthDate">Data urodzenia:</label>
-            <label>*Płeć:</label>
-            <label htmlFor="phoneNumber">Numer telefonu:</label>
-            <label htmlFor="email">*E-mail:</label>
-            <label htmlFor="password">*Hasło:</label>
-            <label htmlFor="confirmPassword">*Powtórz hasło:</label>
-            </div>
-            <div className='bottom-labels'>
-            
-            </div>
-          </div>
-
-          <div className="register-inputs">
-            <div className='top-inputs'>
-            <input type="text" id="firstName" name="firstName" className='one-register-input' required />
-            <input type="text" id="lastName" name="lastName" className='one-register-input' required />
-            <input type="text" id="address" name="address" className='one-register-input' />
-            <input type="date" id="birthDate" name="birthDate" className='one-register-input' />
+        <table>
+        <tr>
+            <td class="right-align">
+              <label htmlFor="firstName">*Imię:</label>
+            </td>
+            <td class="center-align">
+              <input type="text" id="firstName" name="firstName" className='one-register-input' required />
+            </td>
+        </tr>
+        <tr>
+            <td class="right-align">
+              <label htmlFor="lastName">*Nazwisko:</label>
+            </td>
+            <td class="center-align">
+              <input type="text" id="lastName" name="lastName" className='one-register-input' required />
+            </td>
+        </tr>
+        <tr>
+            <td class="right-align">
+              <label htmlFor="address">Adres:</label>
+            </td>
+            <td class="center-align">
+              <input type="text" id="address" name="address" className='one-register-input' />
+            </td>
+        </tr>
+        <tr>
+            <td class="right-align">
+              <label htmlFor="birthDate">*Data urodzenia:</label>
+            </td>
+            <td>
+              <input type="date" id="birthDate" name="birthDate" className='one-register-input' />
+            </td>
+        </tr>
+        <tr>
+            <td className="right-align">
+              <label>*Płeć:</label>
+            </td>
+            <td>
             <div className="gender-options">
               <div>
               <input type="radio" id="genderFemale" name="gender" value="k" required />
@@ -45,20 +59,44 @@ function Register() {
               <label htmlFor="genderMale">M</label>
               </div>
             </div>
-            <input type="tel" id="phoneNumber" name="phoneNumber" className='one-register-input' />
-            <input type="email" id="email" name="email" className='one-register-input' required />
-            <input type="password" id="password" name="password" className='one-register-input' required />
-            <input type="password" id="confirmPassword" name="confirmPassword" className='one-register-input' required />
-            </div>
-           
-            <div className='bottom-inputs'>
-           
-            </div>
-            <GreenButton type="submit" onClick={1+1} >Utwórz konto</GreenButton>
-          </div>
-          </div>
-        
-          
+            </td>
+        </tr>
+        <tr>
+            <td class="right-align">
+              <label htmlFor="phoneNumber">*Numer telefonu:</label>
+            </td>
+            <td class="center-align">
+              <input type="tel" id="phoneNumber" name="phoneNumber" className='one-register-input' />
+            </td>
+        </tr>
+        <tr>
+            <td class="right-align">
+              <label htmlFor="email">*E-mail:</label>
+            </td>
+            <td class="center-align">
+              <input type="email" id="email" name="email" className='one-register-input' required />
+            </td>
+        </tr>
+        <tr>
+            <td class="right-align">
+              <label htmlFor="password">*Hasło:</label></td>
+            <td class="center-align">
+              <input type="password" id="password" name="password" className='one-register-input' required /></td>
+        </tr>
+        <tr>
+            <td class="right-align">
+              <label htmlFor="confirmPassword">*Powtórz hasło:</label></td>
+            <td class="center-align">
+              <input type="password" id="confirmPassword" name="confirmPassword" className='one-register-input' required />
+            </td>
+        </tr>
+        <tr>
+            <td class="right-align"></td>
+            <td class="center-align">
+              <GreenButton type="submit" onClick={1+1} >Utwórz konto</GreenButton>
+            </td>
+        </tr>
+    </table>
         </form>
         </OrangeBackground>
         

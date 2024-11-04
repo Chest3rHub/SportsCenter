9e15ea057ce8b62342189c8a62812474f0924722
Szymon Sawicki <darhmoule@interia.pl>
2024-11-04 22:50:44 +0100

    [BE] Fixed error in tests: FakeUserRepositories

--- backend/SportsCenterBackend/SportsCenter.Tests.Unit/TestObjects/FakeUserRepository.cs
+++ backend/SportsCenterBackend/SportsCenter.Tests.Unit/TestObjects/FakeUserRepository.cs
@@ -32,4 +32,32 @@ public class FakeUserRepository : IUserRepository
         _klienci.Add(client);
         return Task.CompletedTask;
     }
+
+    public Task<Klient?> GetClientByIdAsync(int id, CancellationToken cancellationToken)
+    {
+        // nie testowalem, wrzucam zeby usunac bledy z projektu
+        return Task.FromResult(_klienci.FirstOrDefault(e => e.KlientId == id));
+    }
+
+    public Task<Klient?> GetClientByEmailAsync(string email, CancellationToken cancellationToken)
+    {
+        // nie testowalem, wrzucam zeby usunac bledy z projektu
+        return Task.FromResult(
+            _klienci
+                .Join(
+                    _osoby,
+                    klient => klient.KlientId,
+                    osoba => osoba.OsobaId,
+                    (klient, osoba) => new { Klient = klient, Osoba = osoba }
+                )
+                .Where(joined => joined.Osoba.Email == email)
+                .Select(joined => joined.Klient)
+                .SingleOrDefault()
+        );
+
+    }
+    public Task UpdateClientAsync(Klient client, CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
 }

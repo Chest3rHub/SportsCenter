19325de86273faf69e15e029ca3dab774c8e8a5f
Chest3rHub <s24684@pjwstk.edu.pl>
2025-03-09 00:32:59 +0100

    [FE] Added MUI to few basic components

--- frontend/src/components/GreenBackground.js
+++ frontend/src/components/GreenBackground.js
@@ -1,11 +1,31 @@
-import '../styles/greenBackground.css';
+import { Box } from '@mui/material';
 
-export default function GreenBackground({children, height, marginTop}){
-    return(
-        <div className='green-background' style={{minHeight: height, marginTop: marginTop,}}>
-            <div className='centered-content'>
-              {children}
-            </div>
-        </div>
-    );
+export default function GreenBackground({ children, height, marginTop }) {
+  return (
+    <Box
+      sx={{
+        backgroundColor: '#ebf9ea',
+        borderRadius: '20px',
+        boxShadow: '0 10px 20px rgba(0, 0, 0, 0.2)',
+        width: '60%',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        paddingTop: '7vh',
+        paddingBottom: '3vh',
+        minHeight: height,
+        marginTop: marginTop,
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          flexGrow: 1,
+        }}
+      >
+        {children}
+      </Box>
+    </Box>
+  );
 }

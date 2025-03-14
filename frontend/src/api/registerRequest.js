82b92b37a5c95b9a47d485dd59889c7cab468b68
Chest3rHub <s24684@pjwstk.edu.pl>
2025-03-14 23:57:04 +0100

    [FE] Refactored register request after api for registration changed

--- frontend/src/api/registerRequest.js
+++ frontend/src/api/registerRequest.js
@@ -1,19 +1,19 @@
 import API_URL from "../appConfig";
 
 export default async function registerRequest(formData){
-    return fetch(`${API_URL}/clients`, { 
+    return fetch(`${API_URL}/clients/register-client`, { 
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          imie: formData.firstName,
-          nazwisko: formData.lastName,
-          email: formData.email,
-          haslo: formData.password,
-          dataUr: formData.birthDate,
-          nrTel: formData.phoneNumber,
-          adres: formData.address,
+          Name: formData.firstName,
+          Surname: formData.lastName,
+          Email: formData.email,
+          Password: formData.password,
+          BirthDate: formData.birthDate,
+          PhoneNumber: formData.phoneNumber,
+          Address: formData.address,
         }),
       });
 }

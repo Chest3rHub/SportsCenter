5e2f610ac4861188cb2ece2ad4414cf74527bbeb
Oliwia Dębała <s24416@pjwstk.edu.pl>
2025-03-14 13:22:49 +0100

    [DB] Added seedData script to project

---
+++ backend/database-data/seedData.sql
@@ -0,0 +1,14 @@
+--insert roles into table TypPracownika
+insert into TypPracownika values('Wlasciciel');
+insert into TypPracownika values('Pracownik administracyjny');
+insert into TypPracownika values('Trener');
+insert into TypPracownika values('Pomoc sprzatajaca');
+
+--insert sportsClub working hours into table GodzinyPracyKlubu
+insert into GodzinyPracyKlubu values ('10:00', '22:00', 'poniedzialek')
+insert into GodzinyPracyKlubu values ('10:00', '22:00', 'wtorek')
+insert into GodzinyPracyKlubu values ('10:00', '22:00', 'sroda')
+insert into GodzinyPracyKlubu values ('10:00', '22:00', 'czwartek')
+insert into GodzinyPracyKlubu values ('10:00', '22:00', 'piatek')
+insert into GodzinyPracyKlubu values ('10:00', '22:00', 'sobota')
+insert into GodzinyPracyKlubu values ('10:00', '22:00', 'niedziela')

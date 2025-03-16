7c89d0804cecc2daf95da273e74223d9343552b9
Chest3rHub <s24684@pjwstk.edu.pl>
2025-03-16 21:36:19 +0100

    [FE] Fixed instant displaying of the sad emoji while waiting for the response

--- frontend/src/pages/News.js
+++ frontend/src/pages/News.js
@@ -1,6 +1,6 @@
 import { useEffect, useState, useContext } from 'react';
 import getNews from '../api/getNews';
-import { Typography, Box, Avatar } from '@mui/material';
+import { Typography, Box, Avatar , CircularProgress} from '@mui/material';
 import SingleNews from '../components/SingleNews';
 import GreenBackground from '../components/GreenBackground';
 import Header from '../components/Header';
@@ -12,6 +12,7 @@ export default function News() {
     const { dictionary, toggleLanguage } = useContext(SportsContext);
 
     const [news, setNews] = useState([]);
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
         getNews()
             .then(response => {
@@ -21,6 +22,7 @@ export default function News() {
                 console.log('Odpowiedź z API:', data);
                 // linijka set news ustawia na te pobrane z api, jak wykomentujecie ją to pojawi się ta smutna buzka xd
                 setNews(data);
+                setLoading(false);
             })
             .catch(error => {
                 console.error('Błąd podczas wywoływania getNews:', error);
@@ -32,40 +34,45 @@ export default function News() {
     // max 3 newsy na stronie poki co
     const limitedNews = news.slice(0, 3);
     return (
-
         <GreenBackground gap={"4vh"}>
-            <Header >{dictionary.newsPage.newsLabel}</Header>
+            <Header>{dictionary.newsPage.newsLabel}</Header>
 
-            {news.length === 0 && (<>
+            {loading ? (
                 <Box sx={{ textAlign: 'center', display: "flex", justifyContent: "center" }}>
-                    <Avatar sx={{ width: "20rem", height: "20rem" }}>
-                        <SentimentDissatisfiedIcon sx={{ fontSize: "20rem", color: '#F46C63', stroke: "#ebf9ea", strokeWidth: 1.1, backgroundColor: "#ebf9ea" }} />
-                    </Avatar>
-
-                </Box><Box>
-                    <Typography sx={{
-                        marginTop: "-4vh",
-                        fontSize: "2rem",
-                        fontWeight: "bold",
-                        color: "#F46C63",
-                        textShadow: '0 5px 5px rgba(0, 0, 0, 0.1)',
-
-                    }}>{dictionary.newsPage.nothingHappenedLabel}</Typography>
-                    <Typography sx={{
-                        fontSize: "2rem",
-                        fontWeight: "bold",
-                        color: "#F46C63",
-                        textShadow: '0 5px 5px rgba(0, 0, 0, 0.1)',
-                    }}>{dictionary.newsPage.checkAgainSoonLabel}</Typography>
+                    <CircularProgress sx={{ fontSize: '5rem', color: "#4caf50" }} /> 
                 </Box>
-            </>
+            ) : (
+                <>
+                    {news.length === 0 && (
+                        <>
+                            <Box sx={{ textAlign: 'center', display: "flex", justifyContent: "center" }}>
+                                <Avatar sx={{ width: "20rem", height: "20rem" }}>
+                                    <SentimentDissatisfiedIcon sx={{ fontSize: "20rem", color: '#F46C63', stroke: "#ebf9ea", strokeWidth: 1.1, backgroundColor: "#ebf9ea" }} />
+                                </Avatar>
+                            </Box>
+                            <Box>
+                                <Typography sx={{
+                                    marginTop: "-4vh",
+                                    fontSize: "2rem",
+                                    fontWeight: "bold",
+                                    color: "#F46C63",
+                                    textShadow: '0 5px 5px rgba(0, 0, 0, 0.1)',
+                                }}>{dictionary.newsPage.nothingHappenedLabel}</Typography>
+                                <Typography sx={{
+                                    fontSize: "2rem",
+                                    fontWeight: "bold",
+                                    color: "#F46C63",
+                                    textShadow: '0 5px 5px rgba(0, 0, 0, 0.1)',
+                                }}>{dictionary.newsPage.checkAgainSoonLabel}</Typography>
+                            </Box>
+                        </>
+                    )}
 
-
+                    {limitedNews.map((oneNews) => (
+                        <SingleNews key={oneNews.id} oneNewsDetails={oneNews} />
+                    ))}
+                </>
             )}
-
-            {limitedNews.map((oneNews) => (
-                <SingleNews oneNewsDetails={oneNews} />
-            ))}
         </GreenBackground>
     );
 }

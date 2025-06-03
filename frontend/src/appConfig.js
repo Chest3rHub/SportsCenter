b518ab908260c8d9ccc5ecb2b1bfa7ba38ed0534
Chest3rHub <s24684@pjwstk.edu.pl>
2025-06-03 17:12:39 +0200

    [FE] Live api URL

--- frontend/src/appConfig.js
+++ frontend/src/appConfig.js
@@ -1,5 +1,9 @@
 
-// Szymon 
-const API_URL =  'http://localhost:5277';
+// local URL 
+//const API_URL =  'http://localhost:5277';
+
+
+// URL Live demo API
+const API_URL =  'https://inzynierka-testapi.azurewebsites.net';
 
 export default API_URL;

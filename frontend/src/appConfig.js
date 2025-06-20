a3cb0373e954abb0e334ca9807dc9d4093fae187
Chest3rHub <s24684@pjwstk.edu.pl>
2025-06-20 11:52:48 +0200

    [FE] Config fix

--- frontend/src/appConfig.js
+++ frontend/src/appConfig.js
@@ -1,9 +1,9 @@
 
 // local URL 
-const API_URL =  'http://localhost:5277';
+//const API_URL =  'http://localhost:5277';
 
 
 // URL Live demo API
-//const API_URL =  'https://inzynierka-testapi.azurewebsites.net';
+const API_URL =  'https://inzynierka-testapi.azurewebsites.net';
 
 export default API_URL;
